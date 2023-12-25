5ccf07102a3c57c42511d2611feab069cb2428e1
Tero Keski-Valkama <tero.keskivalkama@gmail.com>
2023-12-25 12:33:43 +0100

    Basic API client.

--- python/recursive_self_improvement_suite/recursive_self_improvement_suite.py
+++ python/recursive_self_improvement_suite/recursive_self_improvement_suite.py
@@ -1,1 +1,40 @@
 """Main module."""
+
+import openai
+import json
+import time
+
+with open('apikey.json', 'r') as apikey_file:
+  config = json.load(apikey_file)
+  openai.api_key  = config['apikey']
+  openai.organization = config['org']
+  model = config['model']
+
+def chat(messages):
+    while (trials < 5):
+        try:
+            completion = openai.ChatCompletion.create(
+                model=model,
+                messages=messages,
+                max_tokens=2,
+                temperature=0.1
+            )
+            print(completion)
+        except Exception as e:
+            print(e)
+            time.sleep(10)
+            trials = trials + 1
+            # Open AI rate limit of one request per second, 60 / minute.
+            time.sleep(2)
+    return completion
+
+
+prompt = [{
+  "role": "system",
+  "content": """\
+"""
+},
+{
+  "role": "user",
+  "content": """"\
+"""}]
